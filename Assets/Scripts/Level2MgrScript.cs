996a66c10815ac472631d18094de3806fc406aa5
jonathanghoma <91575899+jonathanghoma@users.noreply.github.com>
2023-04-12 21:48:00 -0400

    Added timer to levels + level managers

--- Assets/Scripts/Level2MgrScript.cs
+++ Assets/Scripts/Level2MgrScript.cs
@@ -5,9 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class Level2MgrScript : MonoBehaviour
 {
+    private TimerScript _timer;
     void Start()
     {
         Messenger.AddListener(Messages.LEVEL_TRANSFER, changeLevel);
+        _timer = GameObject.FindObjectOfType<TimerScript>();
     }
 
     // Update is called once per frame
@@ -18,6 +20,7 @@ public class Level2MgrScript : MonoBehaviour
 
     void changeLevel()
     {
+        PlayerPrefs.SetFloat("Level2Time", _timer._elapsedTime);
         Messenger.RemoveListener(Messages.LEVEL_TRANSFER, changeLevel);
         SceneManager.LoadScene(0);
     }
