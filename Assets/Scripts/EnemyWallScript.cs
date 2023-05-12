ffdc0fdde959f1752e16674bf76c9719b3ca4339
dpatience627 <dpatience627@gmail.com>
2023-05-12 16:10:27 -0400

    wall police

---
+++ Assets/Scripts/EnemyWallScript.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyWallScript : MonoBehaviour
+{
+    [SerializeField]
+    private List<Transform> _waypoints;
+    private int _target;
+    private NavMeshAgent agent;
+
+
+    public GameObject player;
+    public LayerMask _player;
+    public bool alert = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.SetDestination(_waypoints[0].position);
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    public void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.tag == "waypoint")
+
+        {
+            _target++;
+            if (_target == _waypoints.Count)
+            {
+                _waypoints.Reverse();
+                _target = 1;
+            }
+            agent.SetDestination(_waypoints[_target].position);
+        }
+    }
+
+}
+
+
+
+
